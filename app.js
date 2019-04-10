0f77b163f4fc1804798342fb6d5f01c06ffcc5ed
Hugh Caluscusin <hugh@melodiccrypter.com>
2019-04-10 23:25:42 +0800

    added and configured HPP package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
 const cors = require('cors');
+const hpp = require('hpp');
 const es6Renderer = require('express-es6-template-engine');
 
 // Local App Modules
@@ -21,10 +22,15 @@ app.engine('html', es6Renderer);
 app.set('views', 'views');
 app.set('view engine', 'html');
 
-// Middlewares
+// Middlewares ===> Security
 app.use(helmet({ dnsPrefetchControl: { allow: true } })); // => Helmet, for security of HTTP requests
 app.use(cors({ origin: false })); // => Cross-Origin Resource Sharing is disabled
-app.use(express.json()); // => Parser
+// => some Security middlewares require to be parsed first
+app.use(express.json()); // => Parser for JSON
+app.use(express.urlencoded()); // => Parser for x-www-form-urlencoded
+// Middlewares ===> Security continuation
+app.use(hpp()); // => protection against Parameter Pollution attacks
+// Middlewares ===> Other
 app.use(express.static(publicPath)); // => Static Assets
 app.use(logger(logsPath + '/app-logs.json')); // => Logs, using Winston & Express-Winston
 
